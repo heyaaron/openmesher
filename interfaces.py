9b20ffa1d3e90bff6fd2cc803f7ccaff440df4f1
Aaron C. de Bruyn <aaron@heyaaron.com>
2011-09-03 21:36:28 -0700

    Initial draft of new interfaces

--- interfaces.py
+++ interfaces.py
@@ -10,9 +10,33 @@ class IOpenMesherPlugin(IPlugin):
         return {}
 
 
-class IOpenMesherPluginTest(IPlugin):
-    def process(self):
+class IOpenMesherConfigPlugin(IPlugin):
+    """Interface for configuration plugins.  Accepts a mesh object, returns a dictionary of filenames and contents"""
+    
+    def process(self, mesh, **kwargs):
         """ Begin plugin processing """
+        pass
+    
+    def files(self):
+        """ Return a dictionary of routers containing a dictionary of filenames and contents """
+        return {}
+    
+    def service_to_restart(self):
+        """ Returns a string containing the name of a service to restart, such as 'openvpn'"""
+        return ''
+
+
+class IOpenMesherPackagingPlugin(IPlugin):
+    """
+        Interface for packaging plugins.  Accepts a mesh object and some basic
+        packaging parameters and then returns a dictionary of routers containing
+        a dictionary of files and deployment actions for those files
+    """
+    
+    def process(self, mesh, pkgauthor = 'aaron@heyaaron.com', pkgversion = '1.0', **kwargs):
+        """
+            Perform the actual work of creating package files and building the packages/
+        """
         pass
     
     def files(self):
@@ -24,3 +48,32 @@ class IOpenMesherPluginTest(IPlugin):
         return ''
     
     #TODO: Need to output the folder containing files that makedebs needs to collect
+
+
+class IOpenMesherDeploymentPlugin(IPlugin):
+    """
+        Interface for deployment plugins.  Accepts a mesh object and some basic
+        deployment parameters and then performs the deployment, returning a dictionary
+        of routers and any appropriate info--such as stdout/stderr or just OK/Fail, etc...
+    """
+    
+    def canrestart(self):
+        """
+            Is the plugin able to restart services?
+        """
+    
+    def canreboot(self):
+        """
+            Is the plugin able to reboot the server?
+        """
+    
+    def deploy(self, deploydict, stoponfailure=True):
+        """
+            Performs the actual deployment to a system.
+        """
+        pass
+    
+    def service_to_restart(self):
+        """ Returns a string containing the name of a service to restart, such as 'openvpn'"""
+        return ''
+
