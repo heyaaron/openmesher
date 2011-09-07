7084012d7617b50778c9593bf6c1dd24a9ec4c6e
Aaron C. de Bruyn <aaron@utilizeit.com>
2011-09-07 12:55:46 -0700

    Updated shorewall to run with templating

--- plugins/shorewall/__init__.py
+++ plugins/shorewall/__init__.py
@@ -1,16 +1,18 @@
 import logging, interfaces, os
 from StringIO import StringIO
 from datetime import datetime
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, ChoiceLoader
 
 
 class Shorewall(interfaces.IOpenMesherConfigPlugin):
     def __init__(self):
-        env = Environment(loader=FileSystemLoader('/home/aaron/code/openmesher/plugins/shorewall'))
-        
+        env = Environment(loader=ChoiceLoader([
+                FileSystemLoader('~/.openmesher/'),
+                FileSystemLoader('%s/plugins/' %(os.getcwd())),
+            ]))
         self._files = {}
-        self.interfaces_template = env.get_template('interfaces.conf')
-        print self.interfaces_template
+        self._interfaces_template = env.get_template('shorewall/interfaces.conf')
+        self._rules_template = env.get_template('shorewall/rules.conf')
     
     def process(self, mesh, cliargs=None):
         logging.debug('Generating Shorewall config...')
@@ -20,15 +22,9 @@ class Shorewall(interfaces.IOpenMesherConfigPlugin):
             interfaces = StringIO()
             rules = StringIO()
             
+            self._files[router]['/shorewall/interfaces.mesh'] = self._interfaces_template.render(links = mesh.links[router], zone='vpn')
+            self._files[router]['/shorewall/rules.mesh'] = self._rules_template.render(links= mesh.links[router])
             
-            logging.debug(self.interfaces_template.render(links = mesh.links[router], zone='vpn'))
-            
-            for link in mesh.links[router]:
-                interfaces.write('vpn\t%s\n' %(link.iface))
-                rules.write('ACCEPT\twan\tfw\ttcp\t%s\n' %(link.port))
-                rules.write('ACCEPT\twan\tfw\tudp\t%s\n' %(link.port))
-            self._files[router]['/shorewall/rules.mesh'] = rules.getvalue()
-            self._files[router]['/shorewall/interfaces.mesh'] = interfaces.getvalue()
         return True
     
     def files(self):
