574f391cd793993ad1063993b95312a6451c8506
Aaron C. de Bruyn <aaron@utilizeit.com>
2011-09-07 11:29:44 -0700

    Initial template testing with Shorewall plugin

--- plugins/shorewall/__init__.py
+++ plugins/shorewall/__init__.py
@@ -1,10 +1,16 @@
 import logging, interfaces, os
 from StringIO import StringIO
 from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
+
 
 class Shorewall(interfaces.IOpenMesherConfigPlugin):
     def __init__(self):
+        env = Environment(loader=FileSystemLoader('/home/aaron/code/openmesher/plugins/shorewall'))
+        
         self._files = {}
+        self.interfaces_template = env.get_template('interfaces.conf')
+        print self.interfaces_template
     
     def process(self, mesh, cliargs=None):
         logging.debug('Generating Shorewall config...')
@@ -13,6 +19,9 @@ class Shorewall(interfaces.IOpenMesherConfigPlugin):
             self._files[router] = {}
             interfaces = StringIO()
             rules = StringIO()
+            
+            
+            logging.debug(self.interfaces_template.render(links = mesh.links[router], zone='vpn'))
             
             for link in mesh.links[router]:
                 interfaces.write('vpn\t%s\n' %(link.iface))
